1ddaab2feb98062c29c0c86663021fe24e5c7bb3
liuxiang <liuxiang@umeng.com>
2016-05-26 22:58:52 +0800

    Add org-yank-image layer

---
+++ org-yank-image/local/org-yank-image/org-yank-image.el
@@ -0,0 +1,51 @@
+;;;###autoload
+(defun org-yank-image/default-dir ()
+  (format "%s/media" (file-name-base (buffer-file-name))))
+
+;;;###autoload
+(defun org-yank-image/default-file (dir)
+  (let ((last 0))
+    (format "image%d.png" (1+
+                           (dolist (f (file-expand-wildcards (format "%s/*" dir)) last)
+                             (when (string-match "/image\\([0-9]+\\)\\.png$" f)
+                               (let ((n (string-to-number (match-string 1 f))))
+                                 (when (> n last)
+                                   (setq last n)))))))))
+
+;;;###autoload
+(defun org-yank-image/get-file ()
+  (let* ((default-dir (org-yank-image/default-dir))
+         (default-file (org-yank-image/default-file default-dir)))
+    (format "./%s" (file-relative-name (read-file-name "Save to file: " default-dir nil nil default-file) default-directory))))
+
+;;;###autoload
+(defun org-yank-image/write-to-file (img-data path)
+  (make-directory (file-name-directory path) t)
+  (let* ((temp-file (make-temp-file "org-yank-image"))
+         (cmd (format "sips %s --setProperty format %s --out %s" temp-file (file-name-extension path) (shell-quote-argument (expand-file-name path)))))
+    (f-write-bytes img-data temp-file)
+    (call-process-shell-command cmd)
+    (delete-file temp-file)))
+
+;;;###autoload
+(defun org-yank-image/insert-link (file-path)
+  (insert (format "[[%s]]" file-path))
+  )
+
+;;;###autoload
+(defun org-yank-image/write-and-insert (img)
+  (let* ((file-path (org-yank-image/get-file)))
+    (org-yank-image/write-to-file (plist-get (cdr img) :data) file-path)
+    (org-yank-image/insert-link file-path)
+    (org-display-inline-images nil)))
+
+;;;###autoload
+(defun org-yank-image/yank ()
+  (interactive)
+  (let* ((pboard-item (current-kill 0 t))
+         (img (get-text-property 0 'display pboard-item)))
+    (if img
+        (org-yank-image/write-and-insert img)
+      (call-interactively 'yank))))
+
+(provide 'org-yank-image)
---
+++ org-yank-image/packages.el
@@ -0,0 +1,72 @@
+;;; packages.el --- org-yank-image layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: 刘向 <liuxiang@liuxiangdeMacBookAir>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `org-yank-image-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `org-yank-image/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `org-yank-image/pre-init-PACKAGE' and/or
+;;   `org-yank-image/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst org-yank-image-packages
+  '((org-yank-image :location local))
+  "The list of Lisp packages required by the org-yank-image layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+(defun org-yank-image/init-org-yank-image ()
+  ;; (add-hook 'org-mode-hook #'(lambda ()
+  ;;                              (define-key org-mode-map (kbd "s-v") #'org-yank-image/yank)))
+  (spacemacs|use-package-add-hook org
+    :post-config
+    (define-key org-mode-map (kbd "s-v") #'org-yank-image/yank))
+  (use-package org-yank-image
+    :defer t
+    :init
+    (autoload 'org-yank-image/yank "org-yank-image")))
+
+;;; packages.el ends here
