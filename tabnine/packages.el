a2e5859bba8c76b852c6f185dc0f4455f384afee
Liu Xiang <liuxiang@ktjr.com>
2019-12-12 12:45:07 +0800

    GIT ON FIRE: 2019-12-12 12:45:07

---
+++ tabnine/packages.el
@@ -0,0 +1,24 @@
+;;; Code:
+
+;;; 声明tabnine这个layer，并安装compnay-tabnine这个库
+(defconst tabnine-packages
+  '(company-tabnine)
+  )
+
+;;; 初始化company-tabnine这个库
+(defun tabnine/init-company-tabnine()
+  (use-package company-tabnine
+    :ensure t
+    :defer t
+    :init
+    :config)
+  )
+
+;;; 配置company-tabnine作为company的后端
+(defun tabnine/post-init-company-tabnine()
+  (with-eval-after-load 'company
+    (add-to-list 'company-backends #'company-tabnine)
+    )
+  )
+
+;;; packages.el ends here
